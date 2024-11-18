cebad4f9031dc1362d587e660ea61d9ad0cd195a
Bodia <=>
2024-11-18 20:58:13 +0200

    Add Bohdan Duda Info

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,10 +4,10 @@ const Body = () => {
   return (
     <main style={styles.body}>
       <div style={styles.card}>
-        <img src={"/avatar-default.png"} alt="John Doe" style={styles.photo} />
+        <img src={"/bohdan_duda.jpg"} alt="Bohdan Duda" style={styles.photo} />
         <div style={styles.textContainer}>
-          <h2 style={styles.name}>John Doe</h2>
-          <p style={styles.position}>Software engineer</p>
+          <h2 style={styles.name}>Bohdan Duda</h2>
+          <p style={styles.position}>QA Engineer</p>
         </div>
       </div>
     </main>
