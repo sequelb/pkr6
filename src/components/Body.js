5766ed93236cb36599859d384fa1ca8c7245f39f
KapitaniukMax <maxkapitaniuk@gmail.com>
2024-11-19 16:58:04 +0200

    add Maksym Kapitaniuk

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,10 +4,10 @@ const Body = () => {
   return (
     <main style={styles.body}>
       <div style={styles.card}>
-        <img src={"/avatar-default.png"} alt="John Doe" style={styles.photo} />
+        <img src={"/maksym_kapitaniuk.jpg"} alt="Maksym Kapitaniuk" style={styles.photo} />
         <div style={styles.textContainer}>
-          <h2 style={styles.name}>John Doe</h2>
-          <p style={styles.position}>Software engineer</p>
+          <h2 style={styles.name}>Maksym Kapitaniuk</h2>
+          <p style={styles.position}>Fullstack Developer</p>
         </div>
       </div>
     </main>
